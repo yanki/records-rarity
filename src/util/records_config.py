beaaca4fd149d74f9da146adf64958885860e420
yanki <myankou@g.clemson.edu>
2014-11-16 17:06:05 -0500

    final script additions to finish the database fill in

--- src/util/records_config.py
+++ src/util/records_config.py
@@ -6,7 +6,8 @@ def ExtractInfo():
     fh = open("info.txt", "r")
     for line in fh:
         rank, album, artist, year, genre = line.split("|")
-        InsertRecord(artist=artist, tracklist=None, genre=genre, 
+        genre = genre.encode('utf-8')
+        InsertRecord(artist=artist, tracklist=None, genre=genre,
             album=album, rarity=0.0, art=None, year=year)
     fh.close()
 
