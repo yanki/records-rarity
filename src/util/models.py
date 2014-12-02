c0a8307a1d63843534a35826c8794f5319d21811
csatter <csatter@g.clemson.edu>
2014-12-02 05:03:10 -0500

    Added User Rarity to models.py

--- src/util/models.py
+++ src/util/models.py
@@ -42,3 +42,41 @@ def InsertWishlist(**args):
     cnx.commit()
     cursor.close()
     cnx.close()
+
+def SearchRecords(**args):
+    cnx = mysql.connector.connect(user=DATABASE_USER, password=DATABASE_PASS,
+            host=DATABASE_LOCATION, database=DATABASE_NAME)
+    cursor = cnx.cursor()
+
+    search_string = args['search_field'] #change to whatever the form is
+    search_result = set()
+
+    for s in search_string.split():
+        query = "SELECT DISTINCT artist,album,genre,year FROM records WHERE\
+ artist LIKE '%" + s + "%' OR album LIKE '%" + s + "%'"
+        cursor.execute(query)
+        data = cursor.fetchall()
+        for d in data:
+            search_result.add(d) 
+
+    #do something with search_result
+
+    cursor.close()
+    cnx.close()
+
+def GenerateRarity(**args):
+    cnx = mysql.connector.connect(user=DATABASE_USER, password=DATABASE_PASS,
+            host=DATABASE_LOCATION, database=DATABASE_NAME)
+    cursor = cnx.cursor()
+
+    query = "SELECT AVG(records.rarity) FROM records,owned_vinyl,described_by WHERE owned_vinyl.username = '%s' AND owned_vinyl.o_id = described_by.o_id AND records.v_id = described_by.v_id;" % (args['username'])
+    cursor.execute(query)
+    avg = cursor.fetchall()[0][0]
+    
+    update = "UPDATE users SET rarity = %s WHERE username = '%s';" % (avg, args['username'])
+    cursor.execute(update)
+    cnx.commit()
+
+    cursor.close()
+    cnx.close()
+
